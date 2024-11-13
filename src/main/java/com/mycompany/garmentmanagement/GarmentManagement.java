44f77c93ef3129bf2b7bd61be33d312da01ebc91
Jahid115501 <hasan23105101002@diu.edu.bd>
2024-11-14 01:59:59 +0600

    Second Commit

--- src/main/java/com/mycompany/garmentmanagement/GarmentManagement.java
+++ src/main/java/com/mycompany/garmentmanagement/GarmentManagement.java
@@ -1,16 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.garmentmanagement;
 
-/**
- *
- * @author U S E R
- */
-public class GarmentManagement {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+class Garment {
+    private String id;
+    private String name;
+    private String description;
+    private String size;
+    private String color;
+    private double price;
+    private int stockQuantity;
+    private Fabric fabric; 
+
+    public Garment(String id, String name, String description, String size, String color, double price, int stockQuantity, Fabric fabric) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.size = size;
+        this.color = color;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+        this.fabric = fabric;
+    }
+
+    public void updateStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public double calculateDiscountPrice(double discountPercentage) {
+        return price - (price * discountPercentage / 100);
+    }
+
+    @Override
+    public String toString() {
+        return "Garment [ID=" + id + ", Name=" + name + ", Size=" + size + ", Color=" + color + ", Price=" + price +
+                ", Stock=" + stockQuantity + ", Fabric=" + fabric.getType() + "]";
     }
 }
